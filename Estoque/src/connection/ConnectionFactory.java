a9200009583bad6ef1a0c0bda7335223ddeb7d60
Luccas Portela <luccasaportela@gmail.com>
2020-12-09 02:45:55 -0300

    Arquivos jar

--- Estoque/src/connection/ConnectionFactory.java
+++ Estoque/src/connection/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	
-	private static final String URL = "jdbc:sqlite:C:/Users/Deliziane/Documents/workspace/GitHub/projeto_estoque/Estoque/db/Estoque.DB";
+	private static final String URL = "jdbc:sqlite:C:/Users/Deliziane/Documents/Estudos/Java_curso/GitHub/projetoEstoque/Estoque/bd/Estoque.DB";
 
 	
 	public static Connection getConnection() {
